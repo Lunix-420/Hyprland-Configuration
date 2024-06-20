13f24a626cf521027e3a078ce3291ffd31318432
Lunix420 <davidhess336@gmx.de>
2024-06-20 02:55:22 +0200

    Started with Waybar

--- scripts/waybar.sh
+++ scripts/waybar.sh
@@ -1,9 +1,34 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 CONFIG="$HOME/.config/hypr/waybar/config"
 STYLE="$HOME/.config/hypr/waybar/style.css"
 
-killall waybar
+is_running() {
+	pgrep -x "waybar" >/dev/null
+}
 
-waybar -c $CONFIG &
--s $STYLE
+toggle() {
+	if is_running; then
+		echo "Hiding Waybar"
+		killall -SIGUSR1 waybar
+	else
+		echo "Showing Waybar"
+		waybar -c "$CONFIG" -s "$STYLE" &
+	fi
+}
+
+reload() {
+	echo "Reloading Waybar"
+	killall waybar
+	waybar -c "$CONFIG" -s "$STYLE" &
+}
+
+MODE=$1
+
+if [[ $MODE == 'toggle' ]]; then
+	toggle
+fi
+
+if [[ $MODE == 'reload' ]]; then
+	reload
+fi
