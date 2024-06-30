d497c251f01de964fd1d45c97f921b3054bc4c7d
Lunix-420 <davidhess336@gmx.de>
2024-06-30 20:40:50 +0200

    Network wifget and bux fixes

--- scripts/waybar.sh
+++ scripts/waybar.sh
@@ -1,26 +1,44 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 CONFIG="$HOME/.config/hypr/waybar/config"
 STYLE="$HOME/.config/hypr/waybar/style.css"
 
-is_running() {
-	pgrep -x "waybar" >/dev/null
+check_reserved() {
+	json=$(hyprctl -j monitors)
+	monitor=$(echo "$json" | jq ".[$id]")
+	reserved_first=$(echo "$monitor" | jq '.reserved')
+	first_numbers=$(echo "$reserved_first" | jq -s '.[0]')
+	top_reserved=$(echo "$first_numbers" | jq -r '.[1]')
+	echo "Top reserved are: ${top_reserved}"
+	if [[ "$top_reserved" == "0" ]]; then
+		echo "Reserved area not found"
+		return 1
+	else
+		echo "Reserved area found"
+		return 0
+	fi
 }
 
 toggle() {
-	if is_running; then
-		echo "Hiding Waybar"
-		killall -SIGUSR1 waybar
+	echo "Toggling Waybar"
+	if check_reserved; then
+		echo "Resetting reserved area"
+		hyprctl keyword monitor DP-1,addreserved,0,0,0,0
+		hyprctl keyword monitor HDMI-A-1,addreserved,0,0,0,0
 	else
-		echo "Showing Waybar"
-		waybar -c "$CONFIG" -s "$STYLE" &
+		echo "Adjusting reserved area"
+		hyprctl keyword monitor DP-1,addreserved,-20,0,0,0
+		hyprctl keyword monitor HDMI-A-1,addreserved,-20,0,0,0
 	fi
+	killall -SIGUSR1 waybar
 }
 
 reload() {
 	echo "Reloading Waybar"
 	killall waybar
 	waybar -c "$CONFIG" -s "$STYLE" &
+	hyprctl keyword monitor DP-1,addreserved,-20,0,0,0
+	hyprctl keyword monitor HDMI-A-1,addreserved,-20,0,0,0
 }
 
 MODE=$1
@@ -30,9 +48,5 @@ if [[ $MODE == 'toggle' ]]; then
 fi
 
 if [[ $MODE == 'reload' ]]; then
-	reload
-fi
-
-if [[ $MODE == 'debug' ]]; then
 	reload
 fi
