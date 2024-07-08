1d725b0d2bcd0ba82f481162c46acb0e15c54e2e
Lunix-420 <davidhess336@gmx.de>
2024-07-08 15:33:46 +0200

    Switching to git branch

--- scripts/alacritty.sh
+++ scripts/alacritty.sh
@@ -2,6 +2,7 @@
 
 CONFIG="$HOME/.config/hypr/alacritty/alacritty.toml"
 
+# Check if -f flag is set and if it is, set custom title
 if [ "$1" == "-f" ]; then
 	alacritty --title "Floating Alacritty" --config-file "$CONFIG"
 else
