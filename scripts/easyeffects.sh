41cb589242d7a4e1f16e407437ba99dc26055156
Lunix-420 <davidhess336@gmx.de>
2024-07-29 02:54:48 +0200

    More stuff

--- scripts/easyeffects.sh
+++ scripts/easyeffects.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-easyeffects --gapplication-service &
+nohup easyeffects --gapplication-service &
---
+++ scripts/fix_dolphin.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+curl -L https://raw.githubusercontent.com/KDE/plasma-workspace/master/menu/desktop /plasma-applications.menu -o $HOME/.config/menus/applications.menu
